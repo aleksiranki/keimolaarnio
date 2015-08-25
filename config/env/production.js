51e4cd8554a299c1edc08b74a505a038d8f689e3
Aleksi Ranki <aleksi.ranki@iki.fi>
2015-08-25 21:11:27 +0300

    MongoDB configuration for heroku

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
+  db: "mongodb://keimolaarnio:t7g$'37h@"r"aN;@ds035553.mongolab.com:35553/heroku_ml12dd4m",
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
